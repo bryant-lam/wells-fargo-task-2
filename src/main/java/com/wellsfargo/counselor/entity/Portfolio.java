ae286929d7278465c8f3ae2e2ac078811e28e151
Bryant Lam <89410481+bryant-lam@users.noreply.github.com>
2025-02-18 13:32:52 -0800

    implemented data models using JPA in Spring

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,39 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Portfolio {
+    
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId", nullable = false)
+    private Client client;
+
+    protected Portfolio() {
+        
+    }
+
+    public Portfolio(Client client) {
+        this.client = client;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
